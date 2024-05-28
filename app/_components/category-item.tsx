1d0d242b6d3d577b43c47489018cc934e476ab05
evidencia <evidenciadavid2016@gmail.com>
2024-05-29 00:58:33 +0100

    feat: add category item and list components

---
+++ app/_components/category-item.tsx
@@ -0,0 +1,18 @@
+import { Category } from "@prisma/client";
+import Image from "next/image";
+
+interface CategoryItemProps {
+  category: Category;
+}
+
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  return (
+    <div className="flex items-center gap-3 rounded-full bg-white px-5 py-3 shadow-md">
+      <Image src={category.imageUrl} alt="logo" width={30} height={30} />
+
+      <span className="text-sm font-semibold">{category.name}</span>
+    </div>
+  );
+};
+
+export default CategoryItem;
