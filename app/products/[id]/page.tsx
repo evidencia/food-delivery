20add91c3508dd2ab59d43879a0d9188d02cc731
evidencia <evidenciadavid2016@gmail.com>
2024-06-07 13:08:18 +0100

    feat: ensure juicesnfrom product restaurants are listed

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -18,20 +18,24 @@ const ProductsPage = async ({ params: { id } }: ProductsPageProps) => {
       restaurant: true,
     },
   });
+
+  if (!product) {
+    return notFound();
+  }
+
   const juices = await db.product.findMany({
     where: {
       category: {
         name: "Sucos",
+      },
+      restaurant: {
+        id: product.restaurant.id,
       },
     },
     include: {
       restaurant: true,
     },
   });
-
-  if (!product) {
-    return notFound();
-  }
 
   return (
     <div>
